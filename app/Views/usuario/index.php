0427b07b157aa87d46fbf7fa273190dd6cf16a9d
Aarón Gastelum <souleater130@hotmail.com>
2022-05-02 17:29:13 -0700

    Añadido Modals para funciones
    Se añadieron los Modals para las funciones de crear, editar y eliminar (Carecen de función)

--- app/Views/usuario/index.php
+++ app/Views/usuario/index.php
@@ -25,13 +25,13 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                
+
                 <div class="container-fluid">
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800">Usuarios</h1>
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <a class="btn btn-success" onclick="abrirModal(0)">Agregar nuevo</a>
+                            <button data-bs-toggle="modal" data-bs-target="#createModal" class="btn btn-success">Agregar un nuevo usuario</button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -58,26 +58,29 @@
                                     </tfoot>
                                     <tbody>
 
-                                    <?php foreach ($usuarios as $usuario) { ?> 
-							
-                                        <tr>
-                                          <td> <?= $usuario['id'] ?> </td>
-                                          <td> <?= $usuario['nombre'] ?> </td> 
-                                          <td> <?= $usuario['domicilio'] ?> </td>
-                                          <td> <?= $usuario['telefono']  ?> </td>
-                                          <td> <?= $usuario['correo']  ?> </td>
-                                          <td> <?= $usuario['id']  ?> </td>
-                                        </tr>
-        
-                                    <?php  } ?> 
-                                    
+                                        <?php foreach ($usuarios as $usuario) { ?>
+
+                                            <tr>
+                                                <td> <?= $usuario['id'] ?> </td>
+                                                <td> <?= $usuario['nombre'] ?> </td>
+                                                <td> <?= $usuario['domicilio'] ?> </td>
+                                                <td> <?= $usuario['telefono']  ?> </td>
+                                                <td> <?= $usuario['correo']  ?> </td>
+                                                <td> <a class="btn btn-warning" onclick="abrirModal(0)">Editar</a>
+                                                    <button data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-danger ">Eliminar</button>
+                                                </td>
+
+                                            </tr>
+
+                                        <?php  } ?>
+
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+
                 <div id="modal_info" class="modal fade"></div>
                 <!-- /.container-fluid -->
 
@@ -99,8 +102,7 @@
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -118,31 +120,125 @@
         </div>
     </div>
 
+    <!-- Modal Create Product-->
+    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agregar un nuevo usuario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Nombre:</label>
+                            <input type="text" class="form-control" id="nombre">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Domicilio:</label>
+                            <input type="text" class="form-control" id="domicilio">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Telefono:</label>
+                            <input type="text" class="form-control" id="telefono">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Correo:</label>
+                            <input type="text" class="form-control" id="correo">
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-success">Crear usuario</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Edit Product-->
+/*
+    <!-- Modal Create Product-->
+    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Agregar un nuevo usuario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Nombre:</label>
+                            <input type="text" class="form-control" id="nombre">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Domicilio:</label>
+                            <input type="text" class="form-control" id="domicilio">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Telefono:</label>
+                            <input type="text" class="form-control" id="telefono">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Correo:</label>
+                            <input type="text" class="form-control" id="correo">
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-success">Crear usuario</button>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    <!-- Modal Delete Product-->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Eliminar</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ¿Seguro que deseas eliminar el usuario seleccionado?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger" onClick=>Eliminar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <!-- Scripts -->
     <?php echo view('plantilla/scripts'); ?>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+    <script>
+        //if(!localStorage.getItem("user")){
+        //  location.href="<?= base_url('usuario/login') ?>";
+        //}
 
-  <script>
+        var user = JSON.parse(localStorage.getItem("user"));
+        $(".nombre-usuario").html(user.nombre);
 
-    //if(!localStorage.getItem("user")){
-    //  location.href="<?= base_url('usuario/login') ?>";
-    //}
-
-    var user = JSON.parse(localStorage.getItem("user"));
-    $(".nombre-usuario").html(user.nombre);
-
-    function signout(){
-      localStorage.removeItem("token");
-      localStorage.removeItem("tipo");
-      localStorage.removeItem("user");
-      localStorage.clear();
-      location.href="pages/sign-in.html"
-    }
-
-
-  </script>
+        function signout() {
+            localStorage.removeItem("token");
+            localStorage.removeItem("tipo");
+            localStorage.removeItem("user");
+            localStorage.clear();
+            location.href = "pages/sign-in.html"
+        }
+    </script>
     <!-- End of Scripts -->
 
 </body>
